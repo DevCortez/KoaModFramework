5eb1d0e59cfc1aad84421b51c9e3e4d4c18b7f1c
Cortez <crtzrms@gmail.com>
2020-10-14 12:54:03 -0300

    Fixed implementation of lua scripts

--- re_mod/dllmain.cpp
+++ re_mod/dllmain.cpp
@@ -12,12 +12,11 @@ using namespace rapidjson;
 
 // Globals
 int lua_state = 0;
+std::map<std::string, std::vector<std::string>> lua_scripts;
 
 // Constants
 const auto NEW_STATE_HOOK_SIGNATURE = "8B4708FF88DC0200008BC75F5E5D5B59";
-const auto KI_LUA_PUSHSTRING = "8B54240885D275108B4C24048B4124891083C008894124C38BC2568D70018BFF8A084084C975F92BC6508B44240C5250E8ABFDFFFF83C40C5EC3CC";
 const auto LUAL_LOADFILE = "8B442408508B4424088B480881C1A802";
-const auto REQUIRE_HOOK = "508D4C241CE8????????6A018D44241C508D4C24";
 const auto REQUIRE_HOOK_RETURN = "E8????????5E5BB8010000005F83C478C36A3D";
 const auto LUA_CALL = "8B44240C8B4C24088B5424046A00505152E8????????83C410C3CCCCCCCCCCCC558BEC83EC6C";
 const auto FREE_STRING = "51578BF98B0785C07468536A248D4C240C32DB";
@@ -119,6 +118,25 @@ void _stdcall _require_hook_end_process(char* pFile) {
 
         // Do stuff with the script
         //*
+        if (lua_scripts.find(current_file) != lua_scripts.end()) {
+            for (auto script : lua_scripts[current_file]) {
+                int load_result = luaL_loadfile(lua_state, script.c_str());
+
+                if (!load_result) {
+                    try {
+                        lua_call(lua_state, 0, 0);
+                    }
+                    catch (const std::exception& e){                        
+                        printf("Failed loading module %s with error\n\t %s\n", script.c_str(), e.what());
+                    }
+                    
+                }
+                else {
+                    printf("Failed loading module %s with code %08X\n", script.c_str(), load_result);
+                }
+            }
+            
+        }
         if (current_file.compare("ConsumableManager") == 0) {
             int result = luaL_loadfile(lua_state, ".\\mods\\name_winz.lua");
             printf("Loaded script with result %x\n", result);
@@ -152,7 +170,7 @@ __declspec(naked) void _require_hook_end() {
 }
 
 void hook_require() {
-    std::vector<unsigned int> signature = create_signature_from_string(REQUIRE_HOOK);
+    std::vector<unsigned int> signature;;
     void* address;
     BYTE opcode;
     DWORD placeholder;
@@ -166,8 +184,7 @@ void hook_require() {
 }
 
 void find_game_functions() {
-    std::vector<unsigned int> signature = create_signature_from_string(KI_LUA_PUSHSTRING);
-    ki_lua_pushstring = (void (*__cdecl)(int state, const char* str))LookupSignature(signature);
+    std::vector<unsigned int> signature;
     signature = create_signature_from_string(LUAL_LOADFILE);
     luaL_loadfile = (int(__cdecl*)(int state, const char* filename))LookupSignature(signature);
     signature = create_signature_from_string(LUA_CALL);
@@ -200,89 +217,107 @@ bool LoadMod(std::string path) {
         // Parse the json
         Document document;
         document.Parse(file_content.c_str());
-        const Value& patches = document["patches"];
+        if (document.HasMember("scripts")) {
+            const Value& scripts = document["scripts"];
 
-        if (!patches.IsArray())
-            return false;
+            if (!scripts.IsArray())
+                return false;
 
-        for (unsigned int index = 0; index < patches.Size(); index++) {
-            const Value& element = patches[index];
-            
-            // Validate json
-            if (!element["description"].IsString()) {
-                printf("Invalid description\n");
-                return false;
-            }
-            if (!element["patch"].IsString()) {
-                printf("Invalid patch\n");
-                return false;
-            }
-            if (!element["signature"].IsString()) {
-                printf("Invalid signature\n");
-                return false;
-            }
-            std::map<std::string, std::tuple<double, unsigned int>> vars;
+            for (int index = 0; index < scripts.Size(); index++) {
+                const Value& element = scripts[index];
 
-            if (element.HasMember("vars"))
-            {
-                if (!element["vars"].IsObject()) {
-                    printf("Invalid vars\n");
+                std::string script_trigger = element["trigger"].GetString();
+                std::string script_path = element["path"].GetString();
+
+                lua_scripts[script_trigger].push_back(script_path);
+            }
+        }
+        
+        if (document.HasMember("patches")) {
+            const Value& patches = document["patches"];
+
+            if (!patches.IsArray())
+                return false;
+
+            for (int index = 0; index < patches.Size(); index++) {
+                const Value& element = patches[index];
+
+                // Validate json
+                if (!element["description"].IsString()) {
+                    printf("Invalid description\n");
+                    return false;
+                }
+                if (!element["patch"].IsString()) {
+                    printf("Invalid patch\n");
+                    return false;
+                }
+                if (!element["signature"].IsString()) {
+                    printf("Invalid signature\n");
                     return false;
                 }
 
-                for (Value::ConstMemberIterator variable = element["vars"].MemberBegin();
-                    variable != element["vars"].MemberEnd();
-                    variable++) {
+                std::map<std::string, std::tuple<double, unsigned int>> vars;
 
-                    std::string variable_name = variable->name.GetString(); 
-                    const Value& variable_buffer = element["vars"][variable_name.c_str()];
-                    double variable_value = variable_buffer[0].GetDouble();
-                    unsigned int variable_type = variable_buffer[1].GetUint();
-                    vars.emplace(variable_name,
-                        std::tuple<double, unsigned int>(variable_value, variable_type));
+                if (element.HasMember("vars"))
+                {
+                    if (!element["vars"].IsObject()) {
+                        printf("Invalid vars\n");
+                        return false;
+                    }
 
-                    printf("Added variable %s with value %f and type %d\n", variable_name.c_str(), 
-                        variable_value, variable_type);
+                    for (Value::ConstMemberIterator variable = element["vars"].MemberBegin();
+                        variable != element["vars"].MemberEnd();
+                        variable++) {
+
+                        std::string variable_name = variable->name.GetString();
+                        const Value& variable_buffer = element["vars"][variable_name.c_str()];
+                        double variable_value = variable_buffer[0].GetDouble();
+                        unsigned int variable_type = variable_buffer[1].GetUint();
+                        vars.emplace(variable_name,
+                            std::tuple<double, unsigned int>(variable_value, variable_type));
+
+                        printf("Added variable %s with value %f and type %d\n", variable_name.c_str(),
+                            variable_value, variable_type);
+                    }
                 }
-            }
 
-            std::string description = element["description"].GetString();
-            std::string signature_buffer = element["signature"].GetString();
-            std::string patch_buffer = element["patch"].GetString();
+                std::string description = element["description"].GetString();
+                std::string signature_buffer = element["signature"].GetString();
+                std::string patch_buffer = element["patch"].GetString();
 
-            std::string signature_string;
-            std::string patch_string;
+                std::string signature_string;
+                std::string patch_string;
 
-            // Clean up strings
-            for (const char character : signature_buffer) {
-                if (character != ' ') {
-                    signature_string += character;
+                // Clean up strings
+                for (const char character : signature_buffer) {
+                    if (character != ' ') {
+                        signature_string += character;
+                    }
                 }
-            }
 
-            for (const char character : patch_buffer) {
-                if (character != ' ') {
-                    patch_string += character;
+                for (const char character : patch_buffer) {
+                    if (character != ' ') {
+                        patch_string += character;
+                    }
                 }
-            }
-            printf("DEBUG %s %s\n", signature_string.c_str(), patch_string.c_str());
+                printf("DEBUG %s %s\n", signature_string.c_str(), patch_string.c_str());
 
-            std::vector<unsigned int> signature;
-            std::vector<unsigned int> patch;
+                std::vector<unsigned int> signature;
+                std::vector<unsigned int> patch;
 
-            while (patch_string.length() > 0) {
-                if (patch_string[0] == '[') {
-                    // Is a variable
-                    unsigned int name_end = patch_string.find(']');
-                    std::string variable_name = patch_string.substr(1,
-                        name_end - 1);
+                while (patch_string.length() > 0) {
+                    if (patch_string[0] == '[') {
+                        // Is a variable
+                        unsigned int name_end = patch_string.find(']');
+                        std::string variable_name = patch_string.substr(1,
+                            name_end - 1);
 
-                    auto [double_value, variable_type] = vars[variable_name];
-                    unsigned int int_value = double_value;
-                    float float_value = double_value;
+                        auto [double_value, variable_type] = vars[variable_name];
+                        unsigned int int_value = double_value;
+                        float float_value = double_value;
 
-                    switch (variable_type)
-                    {
+                        switch (variable_type)
+                        {
                         case 1:
                             patch.push_back(int_value & 0xff);
                             break;
@@ -306,36 +341,37 @@ bool LoadMod(std::string path) {
                                 patch.push_back((*(INT64*)(&double_value) >> (8 * i)) & 0xff);
                             }
                             break;
-                    default:
-                        break;
-                    }
+                        default:
+                            break;
+                        }
 
-                    patch_string.erase(0, name_end + 1);
+                        patch_string.erase(0, name_end + 1);
+                    }
+                    else if (patch_string[0] == '?') {
+                        // Anything bigger than a byte is a mask
+                        patch.push_back(0xFFFFFFFF);
+                        patch_string.erase(0, 2);
+                    }
+                    else {
+                        // Is a normal value
+                        unsigned int value = std::stoul(patch_string.substr(0, 2),
+                            nullptr, 16);
+                        patch.push_back(value);
+                        patch_string.erase(0, 2);
+                    }
                 }
-                else if (patch_string[0] == '?') {
-                    // Anything bigger than a byte is a mask
-                    patch.push_back(0xFFFFFFFF);
-                    patch_string.erase(0, 2);
+
+                signature = create_signature_from_string(signature_string);
+
+                // Look for the signature in the memory
+                void* address = LookupSignature(signature);
+                if (address != 0) {
+                    printf("Address found @ %x\n", (int)address);
+                    WritePatch(address, patch);
                 }
                 else {
-                    // Is a normal value
-                    unsigned int value = std::stoul(patch_string.substr(0, 2), 
-                        nullptr, 16);
-                    patch.push_back(value);
-                    patch_string.erase(0, 2);
+                    return false;
                 }
-            }
-
-            signature = create_signature_from_string(signature_string);
-
-            // Look for the signature in the memory
-            void* address = LookupSignature(signature);
-            if (address != 0) {
-                printf("Address found @ %x\n", (int)address);
-                WritePatch(address, patch);
-            }
-            else {
-                return false;
             }
         }
     }
